2092b9537583f9d3a8579b69b1d72835f053a76b
Jimmi08 <janamicek@gmail.com>
2022-03-31 15:54:52 +0200

    fix for topic pagination

--- e107_plugins/forum/forum_viewtopic.php
+++ e107_plugins/forum/forum_viewtopic.php
@@ -646,6 +646,15 @@ class e107ForumThread
 
 		$totalPosts = $this->threadInfo['thread_total_replies'] + 1; // add +1 for the original post. ie. not a reply.
 		$this->pages = ceil(($totalPosts)  / $this->perPage);
+        
+        if($this->page > $this->pages) {
+            $this->page = $this->pages;
+        }
+        
+        if($this->page < 1) {
+            $this->page = 1;
+        }
+
 		$this->noInc = false;
 	}
 
