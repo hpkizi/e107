8aea4ffb5cea625f3d6db90ffc2a45da0fc06e40
Jimmi08 <janamicek@gmail.com>
2022-04-17 11:08:32 +0200

    update theme folder

--- e107_themes/bootstrap5/theme.php
+++ e107_themes/bootstrap5/theme.php
@@ -10,21 +10,16 @@ if(!defined('e107_INIT'))
 	class theme implements e_theme_render
 	{
 
+
+
         public function init()
         {
 
             e107::lan('theme');
 
             e107::meta('viewport', 'width=device-width, initial-scale=1.0'); // added to <head>
-        //    e107::link('rel="preload" href="{THEME}fonts/myfont.woff2?v=2.2.0" as="font" type="font/woff2" crossorigin');  // added to <head>
-
-            //e107::meta('apple-mobile-web-app-capable','yes');
-
-            if($bootswatch = e107::pref('theme', 'bootswatch', false))
-            {
-                e107::css('url', 'https://bootswatch.com/4/' . $bootswatch . '/bootstrap.min.css');
-                e107::css('url', 'https://bootswatch.com/4/' . $bootswatch . '/bootstrap.min.css');
-            }
+	        // e107::link('rel="preload" href="{THEME}fonts/myfont.woff2?v=2.2.0" as="font" type="font/woff2" crossorigin');  // added to <head>
+            // e107::meta('apple-mobile-web-app-capable','yes');
 
             $login_iframe  = e107::pref('theme', 'login_iframe', false);
 
@@ -37,12 +32,22 @@ if(!defined('e107_INIT'))
 
 
 		/**
+		 * Override how THEME_STYLE is loaded. Duplicates will be automatically removed.
+		 * @return void
+		 */
+		function css()
+		{
+			e107::css('theme', THEME_STYLE);
+			e107::css('theme', 'style.css'); // always load style.css last.
+			e107::css('inline', '#carousel-hero.carousel {   margin-bottom: 80px; }');
+
+		}
+		/**
 		 * @param string $text
 		 * @return string without p tags added always with bbcodes
 		 * note: this solves W3C validation issue and CSS style problems
 		 * use this carefully, mainly for custom menus, let decision on theme developers
 		 */
-
 		function remove_ptags($text = '') // FIXME this is a bug in e107 if this is required.
 		{
 
@@ -72,6 +77,9 @@ if(!defined('e107_INIT'))
 					$style = 'splash';
 					break;
 
+				case 'news_months_menu':
+					$style = 'listgroup';
+				break;
 			}
 
 			echo "\n<!-- tablestyle initial:  style=" . $style . "  mode=" . $mode . "  UniqueId=" . varset($options['uniqueId']) . " -->\n\n";
@@ -106,7 +114,7 @@ if(!defined('e107_INIT'))
 			{
 
 				case 'wmessage':
-				    echo '<div class="jumbotron"><div class="container text-center">';
+				    echo '<div class="p-5"><div class="container text-center">';
 				        if(!empty($caption))
 				        {
 				          echo '<h1 class="display-4">'.$caption.'</h1>';
@@ -131,7 +139,7 @@ if(!defined('e107_INIT'))
 					echo '<div class=" mb-4">';
 					if(!empty($caption))
 					{
-						echo '<h5 >' . $caption . '</h5>';
+						echo '<h5>' . $caption . '</h5>';
 					}
 					echo $text;
 					echo '</div>';
@@ -172,7 +180,7 @@ if(!defined('e107_INIT'))
           
             case 'splash':
 	            echo '<div class="container  justify-content-center text-center my-5" id="'.$mode.'">
-	                 <div class="row  align-items-center">
+	                 <div class="row align-items-center">
 	                 <div class="card card-signin col-md-6 offset-md-3 " id="login-template"><div class="card-body">';
 
                 if(!empty($caption))
